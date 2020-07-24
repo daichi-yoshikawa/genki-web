45694979762e2f2f31897c904082794715f85134
daichi-yoshikawa <daichi.yoshikawa@gmail.com>
2020-07-24 15:02:04 -0700

    Fixed typo; prefi -> prefix

--- app/__init__.py
+++ app/__init__.py
@@ -8,11 +8,10 @@ def create_app():
     app = Flask(__name__)
     flask_env = os.getenv('FLASK_ENV') or 'default'
     app.config.from_object(config[flask_env])
-    app.config.from_object(config[flask_env])
     config[flask_env].init_app(app)
 
     from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint, url_prefi='/')
+    app.register_blueprint(main_blueprint, url_prefix='/')
 
     bundles = {
       'css_head': Bundle(
